ec78f2626b86a555b26357df4f0f88e72e036489
Juul <j.l.peters@student.tue.nl>
2026-05-16 16:56:33 +0200

    fix: deployment

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,7 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
+    basepath: '/owk2_workshop',
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
